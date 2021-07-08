44cd2a8dd17345098bcdf869f25e1c86da3233d2
AnastasiaVerich <a.verich.m@gmail.com>
2021-07-08 20:09:10 +0300

    HW6

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -10,7 +10,7 @@ function HW6() {
         saveState<string>('editable-span-value', value)
     }
     const restore = () => {
-        // setValue()
+         setValue(restoreState<string>('editable-span-value',''))
     }
 
     return (
@@ -29,9 +29,6 @@ function HW6() {
             <SuperButton onClick={save}>save</SuperButton>
             <SuperButton onClick={restore}>restore</SuperButton>
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperEditableSpan/>*/}
             <hr/>
         </div>
     )
