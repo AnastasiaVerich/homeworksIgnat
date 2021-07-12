0c3a0dfa0b149db8bcbc30680bcf88f1514ae3d4
AnastasiaVerich <a.verich.m@gmail.com>
2021-07-12 15:46:44 +0300

    HW13

--- src/p2-homeworks/h13/Request.tsx
+++ src/p2-homeworks/h13/Request.tsx
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {zapros} from './RequestsAPI'
 import axios from "axios";
 
 function Request() {
+    const[value, setValue]=useState(true)
 const col =()=>{
     axios
-        .post('https://neko-cafe-back.herokuapp.com/auth/test',{success: true})
-        .then ((response: any)=> response.data.yourBody)
-        .catch(error => alert(error));
+        .post('https://neko-cafe-back.herokuapp.com/auth/test',{success: value})
+        .then ((response: any)=>response.data)
+        .catch(error => alert(error)
+        );
 }
+
 
     return (
         <div>
-            <input type="checkbox" checked={true}/>
+            <input type="checkbox" checked={value} onClick={()=>setValue(!value)}/>
             <button onClick={col}>click</button>
         </div>
     )
