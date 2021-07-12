ab73802349a51f4d677cd1071b00b0fb796df001
AnastasiaVerich <a.verich.m@gmail.com>
2021-07-12 12:39:20 +0300

    HW12

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 import s from "./HW12.module.css";
+import {useDispatch, useSelector} from "react-redux";
+import SuperSelect from "../h7/common/c5-SuperSelect/SuperSelect";
+import {AppStoreType} from "../h10/bll/store";
+import {changeThemeC} from "./bll/themeReducer";
 
-const themes = ['dark', 'red', 'some'];
-
+const themes = ['dark', 'red', 'some']
 function HW12() {
-    const theme = 'some'; // useSelector
-
-    // useDispatch, onChangeCallback
+    const dispatch = useDispatch()
+    const theme= useSelector<AppStoreType, string>((state)=>state.theme.type)
+   const onChangeCallback=(color: string)=>{
+        dispatch(dispatch(changeThemeC(color)))
+    }
 
     return (
         <div className={s[theme]}>
@@ -14,7 +19,10 @@ function HW12() {
             <span className={s[theme + '-text']}>
                 homeworks 12
             </span>
-
+            <SuperSelect
+                options={themes}
+            value={theme}
+            onChangeOption={onChangeCallback}/>
             {/*should work (должно работать)*/}
             {/*SuperSelect or SuperRadio*/}
 
--- src/p2-homeworks/h12/bll/themeReducer.ts
+++ src/p2-homeworks/h12/bll/themeReducer.ts
@@ -1,14 +1,15 @@
 const initState = {
-
+type: "dark"
 };
 
 export const themeReducer = (state = initState, action: any): any => { // fix any
     switch (action.type) {
-        case "": {
-            return state;
+        case "color": {
+            return {...state,
+            type: action.color};
         }
         default: return state;
     }
 };
 
-export const changeThemeC = (): any => {}; // fix any
+export const changeThemeC = (color: any): any => ({type:"color", color}) ; // fix any
